d24c9a4a393ab267bb5201766e2fd9806b34fa8d
David Siddle <siddav83@gmail.com>
2021-04-29 16:08:22 +0100

    added fetch() method and calling mealDB api

--- mealfinder/script.js
+++ mealfinder/script.js
@@ -1,13 +1,34 @@
 const search = document.getElementById('search');
 const submitBut = document.getElementById('submit')
 const randomBut = document.getElementById('random')
+const results = document.getElementById('result-heading')
+const mealsEl = document.getElementById('meals-el')
 
 
 function searchMeal(e) {
  e.preventDefault()
  const term = search.value
- console.log(term)
+
+ if (term.trim()) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    results.innerHTML = `<h2>Search results for ${term}</h2>`
+    if (data.meals === null) {
+      results.innerHTML = `<h2>Sorry but we could not find what you are looking for</h2>`
+    } else {
+      mealsEl.innerHTML = data.meals.map(meal =>
+
+        `<div class="meal">
+        <img src="${meal.strMealThumb}" alt="">
+        </div>`
+      );
+    }
+  });
+  } else {
+   alert("Please enter in a valid search!");
+ }
 }
 
-
-submitBut.addEventListener('submit', searchMeal)
+   submitBut.addEventListener('submit', searchMeal)
